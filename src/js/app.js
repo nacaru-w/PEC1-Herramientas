01a5de50c60d758b87864b3f63c646450df93973
nasch92 <ignaciocr92@gmail.com>
2022-11-23 12:49:50 +0100

    Adding sidebar dependency

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+
+// Create sidebarjs instance
+const sidebarjs = new SidebarElement({
+    /* 
+     * All available options:
+     * https://github.com/SidebarJS/sidebarjs#options 
+     */
+});
+
+// toggle sidebarjs on click
+document
+    .querySelector('.js--sidebarjs-toggle')
+    .addEventListener('click', () => {
+        sidebarjs.toggle();
+    });
